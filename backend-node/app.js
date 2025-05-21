c7db7d870f2d469e3d85b57c03f6232b88d93d7a
eunbie07 <eunbie07@gmail.com>
2025-05-22 02:31:40 +0900

    core

---
+++ backend-node/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+const FASTAPI_URL = 'http://fastapi:8000';
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/api/test', async (req, res) => {
+  try {
+    const response = await axios.get(`${FASTAPI_URL}/ping`);
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).send('FastAPI 연동 에러');
+  }
+});
+
+app.listen(PORT, () => console.log(`Node.js server running on port ${PORT}`));
